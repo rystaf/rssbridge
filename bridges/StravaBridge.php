64231109322d5c2cc8ac7a86f96abc9451da7c3f
Ryan Stafford <ryan@nanorex.net>
2023-06-11 18:05:38 -0400

    [StravaBridge] gpx link, detect parameters, cookie support

--- bridges/StravaBridge.php
+++ bridges/StravaBridge.php
@@ -12,31 +12,64 @@ class StravaBridge extends BridgeAbstract {
             ]
         ],
     ];
+    const CONFIGURATION = [
+        'cookie_string' => [
+            'required' => false,
+        ],
+    ];
+
+    public function detectParameters($url) {
+        if (preg_match('/strava\.com\/athletes\/([\d]+)/', $url, $matches) > 0) {
+            return [
+                'athleteID' => $matches[1]
+            ];
+        }
+        return null;
+    }
 
     public function collectData() {
         $athleteID = $this->getInput('athleteID');
+
+        $descriptions = array();
+        if ($this->getOption('cookie_string')) {
+            $headers = ['Cookie: ' . $this->getOption('cookie_string')];
+            $contents = getContents(self::URI . '/athletes/' . $athleteID, $headers);
+            $dom = str_get_html($contents);
+            $props = $dom->find('div[data-react-class=AthleteProfileHeaderMediaGrid]',0)->{'data-react-props'};
+            $propData = json_decode(html_entity_decode($props));
+            foreach ($propData->items as $item) {
+                $descriptions[$item->activity->id] = $item->activity->description;
+            }
+        }
+
         $dom = getSimpleHTMLDOM(self::URI . '/athletes/' . $athleteID);
         $scriptRegex = "/data-react-props='(.*?)'/";
         preg_match($scriptRegex, $dom, $matches) or returnServerError('Could not find json');
         $jsonData = json_decode(html_entity_decode($matches[1]));
-
-        $this->feedName = $jsonData->athlete->name . ' Recent Activities';
+        $this->feedName = $jsonData->athlete->name . "'s Recent Activities";
         $this->iconURL = $jsonData->athlete->avatarUrl;
         foreach ($jsonData->recentActivities as $activity) {
             $item = array();
+
             $item['title'] = $activity->name . ' (' . $activity->detailedType . ')';
             $item['author'] = $jsonData->athlete->name;
             $item['uri'] = self::URI . '/activities/' . $activity->id;
             $item['timestamp'] = $activity->startDateLocal;
-            $content = '<b>Distance:</b> ' . $activity->distance .
-                '<br><b>Elev Gain:</b> ' . $activity->elevation .
-                '<br><b>Time:</b> ' . $activity->movingTime . '<br><br>';
+
+            $content = "<p>{$descriptions[$activity->id]}</p>" .
+                       "<b>Distance:</b> " . $activity->distance .
+                       "<br><b>Elev Gain:</b> " . $activity->elevation .
+                       "<br><b>Time:</b> " . $activity->movingTime . "<br><br>";
             foreach ($activity->images as $image) {
                 $src = $image->squareSrc;
                 if (empty($src)) { $src = $image->defaultSrc; }
                 $content .= '<img src="' . $src . '">';
             }
             $item['content'] = $content;
+
+            // this link requires login
+            $item['enclosures'][] = $item['uri'] . '/export_gpx';
+
             $this->items[] = $item;
         }
 
