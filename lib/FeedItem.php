cd43db0b19624754cf72b378ffd2c60726976162
Ryan Stafford <ryan@nanorex.net>
2023-10-21 14:39:17 -0400

    don't truncate titles

--- lib/FeedItem.php
+++ lib/FeedItem.php
@@ -138,7 +138,7 @@ class FeedItem
         if (!is_string($title)) {
             Debug::log('Title must be a string!');
         } else {
-            $this->title = truncate(trim($title));
+            $this->title = trim($title);
         }
     }
 
