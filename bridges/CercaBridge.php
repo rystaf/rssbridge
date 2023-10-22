cd9e1598cc2db082330fce0e2cb43e9660c24676
Ryan Stafford <ryan@nanorex.net>
2023-10-22 19:58:48 -0400

    [CercaBridge] working

--- bridges/CercaBridge.php
+++ bridges/CercaBridge.php
@@ -4,6 +4,12 @@ class CercaBridge extends FeedExpander
 {
     const NAME = 'Cerca Bridge';
     const DESCRIPTION = "Returns latest posts";
+    const TIMEFRAME = 48;
+    const MAX_POSTS = 10;
+    const FETCH_DELAY = 5;
+    private $now = false;
+    private $fails = 0;
+    private $posts = Array();
     const PARAMETERS = [
         [
             'domain' => [
@@ -15,17 +21,24 @@ class CercaBridge extends FeedExpander
 
     public function collectData()
     {
-        $this->fails = 0;
-        $this->collectExpandableDatas("https://" . $this->getInput('domain') . "/rss.xml", 5);
+        $this->collectExpandableDatas("https://" . $this->getInput('domain') . "/rss.xml", 10);
+        ksort($this->posts);
+        $posts = array_reverse(array_values($this->posts));
+        $this->items = $posts;
     }
 
     protected function parseItem(array $item)
     {
-        $anchor = "#" . parse_url($item['uri'])["fragment"];
-        $uri = str_replace($anchor, '', $item['uri']);
-        if (preg_match('/\[[0-9\-]+\] (.+) posted/', $item["content"], $matches) > 0) {
-            $item["author"] = $matches[1];
+        $id = parse_url($item['uri'])["fragment"];
+        if (!$this->now) {
+            $this->now = $item['timestamp'];
+        } else {
+            if (((($this->now - $item['timestamp']) / 3600) > self::TIMEFRAME) && count($this->posts) >= self::MAX_POSTS) {
+                return;
+            }
         }
+        $anchor = "#" . $id;
+        $uri = str_replace($anchor, '', $item['uri']);
         try {
             $html = $this->getResetCache($uri);
             $content = $html->find($anchor, 0);
@@ -36,12 +49,29 @@ class CercaBridge extends FeedExpander
             if (!$content) {
                 return;
             }
-            $content->find("section", 0)->remove();
-            $item["content"] = $content->save();
+            foreach (array_reverse($html->find('article')) as $article) {
+                $timestamp = strtotime($article->find('time', 0)->innertext . ' 12:00:00');
+                if (((($this->now - $timestamp) / 3600) > self::TIMEFRAME) && count($this->posts) >= self::MAX_POSTS){
+                    continue;
+                }
+                if (date('d', $timestamp) == date('d', $item["timestamp"])) {
+                    $timestamp = $item["timestamp"];
+                }
+                $author = $article->find('b', 0)->innertext;
+                $body = str_get_html($article->save());
+                $body->find('article',0)->first_child()->remove();
+                $this->posts[(int)$article->id] = [
+                    "uri" => str_replace($anchor, '#'.$article->id, $item['uri']),
+                    "author" => $author,
+                    "title" => $item["title"],
+                    "timestamp" => $timestamp,
+                    "content" => $body->save(),
+                ];
+            }
         } catch (\Exception $e) {
             return;
         }
-        return $item;
+        return;
     }
     private function getResetCache($url, $force = false) {
         $ttl = 86400;
@@ -49,7 +79,7 @@ class CercaBridge extends FeedExpander
         $cacheKey = 'pages_' . $url;
         $content = $cache->get($cacheKey);
         if (!$content || $force) {
-            sleep(15);
+            sleep(self::FETCH_DELAY);
             $content = getContents($url, $header ?? [], $opts ?? []);
         }
         $cache->set($cacheKey, $content, $ttl);
