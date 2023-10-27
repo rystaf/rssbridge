86e9daf36d360b226e202452e3c26f793d034bc7
Ryan Stafford <ryan@nanorex.net>
2023-10-26 21:57:55 -0400

    [NasaApodBridge] custom feed expand

--- bridges/NasaApodBridge.php
+++ bridges/NasaApodBridge.php
@@ -2,47 +2,51 @@
 
 class NasaApodBridge extends BridgeAbstract
 {
-    const MAINTAINER = 'corenting';
     const NAME = 'NASA APOD Bridge';
     const URI = 'https://apod.nasa.gov/apod/';
-    const CACHE_TIMEOUT = 43200; // 12h
-    const DESCRIPTION = 'Returns the 3 latest NASA APOD pictures and explanations';
+    const DESCRIPTION = 'Returns the latest NASA APOD pictures and explanations';
 
     public function collectData()
     {
-        $html = getSimpleHTMLDOM(self::URI . 'archivepix.html');
-
-        // Start at 1 to skip the "APOD Full Archive" on top of the page
-        for ($i = 1; $i < 4; $i++) {
-            $item = [];
-
-            $uri_page = $html->find('a', $i + 3)->href;
-            $uri = self::URI . $uri_page;
-            $item['uri'] = $uri;
-
-            $picture_html = getSimpleHTMLDOM($uri);
-            $picture_html_string = $picture_html->innertext;
-
-            //Extract image and explanation
-            $image_wrapper = $picture_html->find('a', 1);
-            $image_path = $image_wrapper->href;
-            // This image is not present when youtube embed
-            $img_placeholder = $image_wrapper->find('img', 0);
-            $img_alt = $img_placeholder->alt ?? '';
-            $img_style = $img_placeholder->style ?? '';
-            $image_uri = self::URI . $image_path;
-            $new_img_placeholder = "<img src=\"$image_uri\" alt=\"$img_alt\" style=\"$img_style\">";
-            $media = "<a href=\"$image_uri\">$new_img_placeholder</a>";
-            $explanation = $picture_html->find('p', 2)->innertext;
-
-            //Extract date from the picture page
-            $date = explode(' ', $picture_html->find('p', 1)->innertext);
-            $item['timestamp'] = strtotime($date[4] . $date[3] . $date[2]);
-
-            //Other informations
-            $item['content'] = $media . '<br />' . $explanation;
-            $item['title'] = $picture_html->find('b', 0)->innertext;
-            $this->items[] = $item;
+        $xmlString = getContents('https://apod.com/feed.rss');
+        $xmlString = preg_replace('/&(?!#?[a-z0-9]+;)/', '&amp;', $xmlString);
+        $xml = simplexml_load_string(trim($xmlString));
+        foreach ($xml->channel[0]->item as $item) {
+            $this->items[] = $this->parseXmlItem($item);
         }
+    }
+
+    public function getName() {
+        return "Astronomy Picture of the Day";
+    }
+
+    public function getIcon() {
+        return 'https://voyager.jpl.nasa.gov/assets/images/galleries/images-voyager-took/saturn/saturn.gif';
+    }
+
+    private function parseXmlItem($feedItem) {
+        $item = [
+            'uri'           => (string)$feedItem->link,
+            'title'         => (string)$feedItem->title,
+            'content'       => (string)$feedItem->content,
+            'timestamp'     => strtotime((string)$feedItem->pubDate),
+            'author'        => '',
+        ];
+        $namespaces = $feedItem->getNamespaces(true);
+        foreach ($feedItem->children($namespaces['dc'])->creator as $creator) {
+            if (!in_array((string)$creator, ['Robert Nemiroff (MTU)', 'Jerry Bonnell (UMCP)'])) {
+                $item['author'] .= (string)$creator.", ";
+            }
+        }
+        $item['author'] = rtrim($item['author'], ", ");
+        $item['content'] = (string)$feedItem->children($namespaces['content'])->encoded;
+
+        // replace youtube embed with thumbnail link
+        $item['content'] = preg_replace(
+            '/<iframe.+src="https:\/\/www\.youtube.com\/embed\/([a-zA-Z0-9_]+).+><\/iframe>/',
+            '<a href="https://www.youtube.com/watch?v=$1"><img src="https://img.youtube.com/vi/$1/sddefault.jpg"></a>',
+            $item['content']
+        );
+        return $item;
     }
 }
